651409662ad1c4639badfb7c712f94868fff2443
sampson <updateserver@163.com>
2015-05-14 19:11:49 +0800

    init

---
+++ SampWeb/Consts/WebConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampWeb.Consts
+{
+    internal class WebConfig
+    {
+        /// <summary>
+        /// 监听端口
+        /// </summary>
+        internal ushort WebPort { get; set; }/*端口*/
+        /// <summary>
+        /// 网站物理路径
+        /// </summary>
+        internal string PhysicalPath { get; set; }/*路径*/
+        /// <summary>
+        /// 虚拟路径
+        /// </summary>
+        internal string VirtualPath { get; set; }/*虚拟路径*/
+        /// <summary>
+        /// 是否需要请求认证
+        /// </summary>
+        internal bool RequireAuthentication { get; set; }/*请求认证*/
+        /// <summary>
+        /// 是否开启目录浏览
+        /// </summary>
+        internal bool ShowDirectoryList { get; set; }/*目录浏览*/
+    }
+}
---
+++ SampWeb/WebServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampWeb
+{
+    [PermissionSet(SecurityAction.InheritanceDemand, Name = "FullTrust")]
+    public class WebServer : MarshalByRefObject, IDisposable
+    {
+
+
+
+
+
+        #region [重写]
+        public void Dispose()
+        {
+
+        }
+        #endregion
+    }
+}
